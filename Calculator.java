9f4eb66e64f4dc5f55ab20e4712b057583861cd1
Fadri <fadriasliarga@gmail.com>
2026-09-13 10:47:30 +0700

    first hello world in java swing

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Calculator {
+    int boardWidth = 360;
+    int boardHeight = 540;
+
+    JFrame frame = new JFrame("Calculator Java");
+
+    Calculator() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Hello World");
+
+        frame.add(label, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+}
