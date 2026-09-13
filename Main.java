7409954dcfc4168b24a0550c18ee1375960a2189
Fadri <fadriasliarga@gmail.com>
2026-09-13 10:25:05 +0700

    add looping

--- Main.java
+++ Main.java
@@ -4,32 +4,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1. Add");
-        System.out.println("2. Sub");
-        System.out.println("3. Mul");
-        System.out.println("4. Div");
-        System.out.print("Choose num: ");
-        int numMenu = scanner.nextInt();
+        while (true) {
+            System.out.println("1. Add");
+            System.out.println("2. Sub");
+            System.out.println("3. Mul");
+            System.out.println("4. Div");
+            System.out.println("5. Exit");
+            System.out.print("Choose num: ");
+            int numMenu = scanner.nextInt();
 
-        System.out.print("Input num1: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Input num2: ");
-        int num2 = scanner.nextInt();
+            if (numMenu < 5 & numMenu > 0) {
 
-        switch (numMenu) {
-            case 1:
-                System.out.println("Add: " + addNumber(num1, num2));
+                System.out.print("Input num1: ");
+                int num1 = scanner.nextInt();
+                System.out.print("Input num2: ");
+                int num2 = scanner.nextInt();
+
+                switch (numMenu) {
+                    case 1:
+                        System.out.println("Add: " + addNumber(num1, num2));
+                        break;
+                    case 2:
+                        System.out.println("Sub: " + subNumber(num1, num2));
+                        break;
+                    case 3:
+                        System.out.println("Mul: " + mulNumber(num1, num2));
+                        break;
+                    case 4:
+                        System.out.println("Div: " + divNumber(num1, num2));
+                        break;
+                }
+            } else if (numMenu == 5) {
+                System.out.println("Bye!");
                 break;
-            case 2:
-                System.out.println("Sub: " + subNumber(num1, num2));
-                break;
-            case 3:
-                System.out.println("Mul: " + mulNumber(num1, num2));
-                break;
-            case 4:
-                System.out.println("Div: " + divNumber(num1, num2));
-                break;
+            } else {
+                System.out.println("Invalid number menu!");
+            }
+
         }
+        ;
 
         scanner.close();
 
